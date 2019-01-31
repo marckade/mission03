08f33e7a302a78823781b8631ad75d9fcb30cbfd
Kaden Marchetti <marckade@isu.edu>
2019-01-31 14:02:50 -0700

    Inserted all old code from previous missions including the doublyLinkedList and LinkedStack

---
+++ src/main/java/edu/isu/cs/cs3308/structures/impl/LinkedQueue.java
@@ -0,0 +1,51 @@
+package edu.isu.cs.cs3308.structures.impl;
+
+import edu.isu.cs.cs3308.structures.Queue;
+
+public class LinkedQueue<E> implements Queue {
+    
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void offer(Object element) {
+
+    }
+
+    @Override
+    public Object peek() {
+        return null;
+    }
+
+    @Override
+    public Object poll() {
+        return null;
+    }
+
+    @Override
+    public void printQueue() {
+
+    }
+
+    @Override
+    public void transfer(Queue into) {
+
+    }
+
+    @Override
+    public void reverse() {
+
+    }
+
+    @Override
+    public void merge(Queue from) {
+
+    }
+}
